dcb7fe7a3072aa56058ecb1eec05e5e35250e9a6
Sahil Dafade <sahildafade123@gmail.com>
2025-08-08 09:05:21 +0530

    added fe url

--- src/app.js
+++ src/app.js
@@ -16,7 +16,10 @@ const app = express();
 // Security middlewares
 app.use(helmet()); // Secure headers
 
-app.use(cors({ origin: '*', credentials: true }));
+app.use(cors({
+  origin: 'http://localhost:5173', // frontend URL
+  credentials: true
+}));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cookieParser());
